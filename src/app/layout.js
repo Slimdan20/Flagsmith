b8d4104889d258d2bcca4d54e929037a36628a92
daniel efe <daneytechwriter@gmail.com>
2025-08-30 23:00:07 +0100

    new

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { FlagsmithProvider } from "flagsmith/react";
+import flagsmith from "flagsmith";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -19,11 +22,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <FlagsmithProvider
+          options={{ environmentID: "GugcXWy5TPjqqYwPrdZKFj" }}
+          flagsmith={flagsmith}
+        >
+          {children}
+        </FlagsmithProvider>
       </body>
     </html>
   );
 }
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,27 @@
+"use client";
 import Footer from "@/Components/Footer/Footer";
 import Homepage from "@/Components/Homepage/Homepage";
 import Navbar from "@/Components/Navbar/Navbar";
 import Services from "@/Components/Services/Services";
 import Order from "@/Components/Order/Order";
 import About from "@/Components/About/About";
-
+import { useFlagsmith } from "flagsmith/react";
 
 export default function Home() {
+  const flagsmith = useFlagsmith();
+
+  const showOrder = flagsmith.hasFeature("order-page-feature");
+
   return (
     <main>
       <Navbar />
       <Homepage />
-      <Services/>
-      {/*<Order/>*/}
-      <About/>
-      <Footer/>
+      <Services />
+      {showOrder && <Order />}
+      <About />
+      <Footer />
     </main>
   );
 }
+
 
